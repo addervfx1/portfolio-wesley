3f718a2c21ab29f36bd0504ac97bbe69e0aeaef4
Wesley Emerson <87941369+addervfx1@users.noreply.github.com>
2024-04-26 20:20:32 -0300

    Primeira etapa de idealização do portfolio
    Foi alterado de curriculo para Portfolio e criado os arquivos de alguns componentes para ainda serem desenvolvidos

---
+++ src/app/experiencia-profissional/experiencia-profissional.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia-profissional',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiencia-profissional.component.html',
+  styleUrl: './experiencia-profissional.component.scss'
+})
+export class ExperienciaProfissionalComponent {
+
+}
---
+++ src/app/info-pessoais/info-pessoais.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-pessoais',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-pessoais.component.html',
+  styleUrl: './info-pessoais.component.scss'
+})
+export class InfoPessoaisComponent {
+
+}
